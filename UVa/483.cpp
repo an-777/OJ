0d0751d069e600930a7e9c13aa85a8ac21ce8938
an-777 <131597737+an-777@users.noreply.github.com>
2023-09-29 22:53:51 +0800

    Create 483.cpp

---
+++ UVa/483.cpp
@@ -0,0 +1,31 @@
+// Problem: 264 - Count on Cantor
+// Contest: UVa
+// URL: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=424
+// Memory Limit: 
+// Time Limit: 3000 ms
+// O(N)
+
+#include <iostream>
+using namespace std;
+int main(){
+	string s;
+	while(getline(cin,s)){
+		int r,l=0;
+		for(int i=0;i<s.size();i++){
+			if(s[i]==' '){
+				r=i-1;
+				for(int j=r;j>=l;j--){
+					cout<<s[j];
+				}
+				l=r+2;
+				cout<<" ";
+			}else if(i==s.size()-1){
+				r=i;
+				for(int j=r;j>=l;j--){
+					cout<<s[j];
+				}
+			}
+		}
+		cout<<"\n";
+	}
+}
