edee740953fe132282088d3fae92f48a4c5fb39d
an-777 <131597737+an-777@users.noreply.github.com>
2023-10-06 20:00:53 +0800

    Create Drunk Vigenère.cpp

---
+++ Kattis/Drunk Vigenère.cpp
@@ -0,0 +1,20 @@
+// Problem: Drunk Vigenère
+// Contest: Kattis
+// URL: https://open.kattis.com/problems/drunkvigenere?tab=submissionsorg/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=325
+// Memory Limit: 1024 mb
+// Time Limit: 1000 ms
+
+#include <iostream>
+using namespace std;
+int main(){
+    string s1,s2;
+    cin>>s1>>s2;
+    for(int i=0;i<s1.length();i++){
+        if(i%2==0){
+            s1[i]=char((s1[i]-(s2[i]-'A')-'A'+26)%26+'A');
+        }else{
+            s1[i]=char((s1[i]+(s2[i]-'A')-'A'+26)%26+'A');
+        }
+    }
+    cout<<s1;
+    }
