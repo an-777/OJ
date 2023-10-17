cf52d4afd256484dc04420145847ea3e916cdf0d
an-777 <131597737+an-777@users.noreply.github.com>
2023-10-17 21:14:26 +0800

    Create 405.cpp

---
+++ TNFSH_OJ/405.cpp
@@ -0,0 +1,39 @@
+// Problem: 405 / B. 自戀數 (chals)
+// Contest: TNFSH Online Judge
+// URL: https://toj.tfcis.org/oj/chal/172514/
+// Memory Limit: 524288 Kb
+// Time Limit: 500 ms
+
+#include <iostream>
+using namespace std;
+
+int pow(int a,int b){
+	int result=1;
+	for(int i=0;i<b;i++){
+		result*=a;
+	}
+	return result;
+}
+
+int main(){
+	int b,n,t=0,temp,d=0,k;
+	cin>>b>>n;
+	temp=n;
+	while(temp>0){
+		d++;
+		temp/=10;
+	}
+	temp=n;	
+	for(int i=1;temp>0;i*=b){
+		t+=(temp%10)*i;
+		temp/=10;
+	}
+	temp=n;
+	k=0;
+	for(int i=1;i<=d;i++){
+		k+=pow((temp%10),d);
+		temp/=10;
+	}
+	if(k==t) cout<<"YES\n";
+	else cout<<"NO\n";
+}
