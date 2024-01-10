eade09e3d39230c740f4ab879276f6f8d43e31e8
an-777 <131597737+an-777@users.noreply.github.com>
2024-01-10 19:19:27 +0800

    Create 595.cpp

---
+++ TNFSH_OJ/595.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int a, b;
+	cin >> a >> b;
+	for(int i = 5, j = 7; i <= a && j <= b; i+=5, j+=7){
+		cout << i << " " << j << "\n";
+	}
+}
