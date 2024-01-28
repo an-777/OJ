99c79c0adc0ec1c2566ce45dca6f2f3b0d8f1646
an-777 <131597737+an-777@users.noreply.github.com>
2024-01-28 13:20:16 +0800

    Create a233.cpp

---
+++ ZeroJudge/a233.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+	int n;
+	vector<int> ary;
+	cin >> n;
+	ary.clear();
+	ary.resize(n);
+	for(int i = 0 ; i < n ; i++){
+		cin >> ary[i];
+	}
+	sort(ary.begin(),ary.end());
+	for(int i = 0 ; i < n ; i++){
+		cout << ary[i] << " ";
+	}
+	cout << "\n";
+}
